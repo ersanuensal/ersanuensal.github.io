3d9479eeaf9b85ae1d9be18d4d416dd5f1cd3b02
ersanuensal <76909364+ersanuensal@users.noreply.github.com>
2022-01-27 16:10:30 +0100

    New Deployment

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ self.addEventListener('install', function (event) {
     event.waitUntil(
       caches.open('my-cache-v1')
         .then(function (cache) {
-          return cache.addAll(['/', '/index.html', 'service-worker.js']);
+          return cache.addAll(['/', '/index.html', 'service-worker.js', 'site.webmanifest']);
         })
     );
   });
