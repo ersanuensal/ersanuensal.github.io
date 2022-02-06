aa4a7b10994bf4a9ceca9546d20864eb80e0d7d3
ersanuensal <ersan.uensal@stud.fra-uas.de>
2022-02-06 01:23:25 +0100

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'my-site-cache-v4';
+var CACHE_NAME = 'my-site-cache-v5';
 var urlsToCache = [
     '/',
     '/css/styles.css',
@@ -57,7 +57,7 @@ self.addEventListener('fetch', function (event) {
 
 self.addEventListener('activate', function (event) {
 
-    var cacheAllowlist = ['my-site-cache-v4'];
+    var cacheAllowlist = ['my-site-cache-v5'];
 
     event.waitUntil(
         caches.keys().then(function (cacheNames) {
