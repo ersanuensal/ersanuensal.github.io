dae41b0da578d5e630a57a1a87c802edd066d432
ersanuensal <ersan.uensal@stud.fra-uas.de>
2022-02-06 01:22:57 +0100

    added table

--- js/app.js
+++ js/app.js
@@ -11,11 +11,61 @@ if ('serviceWorker' in navigator) {
     });
 }
 
-let myObj = {
-    name: 'John',
-    age: 30,
-    city: 'New York'
-};
+// let myObj = {
+//     name: 'John',
+//     age: 30,
+//     city: 'New York'
+// };
 
-localStorage.setItem('myObj', JSON.stringify(myObj));
-console.log(localStorage)
+// localStorage.setItem('myObj', JSON.stringify(myObj));
+// console.log(localStorage)
+var costList = new Array();
+
+
+
+function addData() {
+    getData();
+    costList.push({
+        description: document.getElementById('description').value,
+        date: document.getElementById('date').value,
+        category: document.getElementById('category').value,
+        value: document.getElementById('value').value
+    });
+
+    localStorage.setItem('localData', JSON.stringify(data));
+
+    console.log("New Item added");
+}
+
+function getData() {
+    var data = localStorage.getItem('localData');
+
+    if (data != null) {
+        costList = JSON.parse(data);
+    }
+}
+
+function deleteData() {
+    localStorage.clear();
+}
+
+function showData() {
+    getData();
+
+    var table = document.getElementById('costTable');
+
+    costList.forEach(element => {
+        var row = table.insertRow();
+        var cell1 = row.insertCell();
+        var cell2 = row.insertCell();
+        var cell3 = row.insertCell();
+        var cell4 = row.insertCell();
+        var cell5 = row.insertCell();
+
+        cell1.innerHTML = element.description;
+        cell2.innerHTML = element.date;
+        cell3.innerHTML = element.category;
+        cell4.innerHTML = element.value;
+        cell5.innerHTML = element.value;
+    });
+}
